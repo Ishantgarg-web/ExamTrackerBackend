c2955287c1f931c4945189410102914544eef4f4
Ishantgarg-web <ishantkumar1412@gmail.com>
2026-01-24 17:07:16 +0530

    Initial commit

---
+++ examTracker/src/main/java/com/example/examTracker/ExamTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.examTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamTrackerApplication.class, args);
+	}
+
+}
---
+++ examTracker/src/test/java/com/example/examTracker/ExamTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

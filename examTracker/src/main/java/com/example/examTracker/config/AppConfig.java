55507e2a96279e669489ab6a4d1596c8c8a14068
Ishantgarg-web <ishantkumar1412@gmail.com>
2026-01-28 20:40:26 +0530

    dummy commit

--- examTracker/src/main/java/com/example/examTracker/config/AppConfig.java
+++ examTracker/src/main/java/com/example/examTracker/config/AppConfig.java
@@ -19,7 +19,7 @@ public class AppConfig {
     @Bean
     public AuthenticationManager authenticationManager(
             AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
+        return config.getAuthenticationManager(); 
     }
 
     @Bean
